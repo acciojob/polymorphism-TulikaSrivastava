13f29d8ad65280a60e939a44aa1d4b91271fecb8
Tulika <sriva0507@gmail.com>
2023-07-31 01:55:12 +0530

    added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,37 +1,24 @@
-class Main {
-    class Product {
-        public int product(int x, int y) {
-            return x * y;
-        }
-
-        public int product(int x, int y, int z) {
-            return x * y * z;
-        }
-
-        public double product(double x, double y) {
-            return x * y;
-        }
+package com.driver;
+public class Main {
+public static class Product {
+    public int product(int x, int y) {
+        return x * y;
     }
 
-    public static void main(String[] args) {
-        Main mainObj = new Main();
-        Main.Product p = mainObj.new Product();
-
-        int result1 = p.product(2, 3);
-        System.out.println("Result 1: " + result1);
-
-        int result2 = p.product(2, 3, 4);
-        System.out.println("Result 2: " + result2);
-
-        double result3 = p.product(2.5, 3.5);
-        System.out.println("Result 3: " + result3);
-
-        // Polymorphism example
-        Product p1 = mainObj.new Product();
-        Product p2 = mainObj.new Product();
-
-        System.out.println("Polymorphism Example:");
-        System.out.println("p1.product(2, 3): " + p1.product(2, 3));
-        System.out.println("p2.product(2.5, 3.5): " + p2.product(2.5, 3.5));
+    public int product(int x, int y, int z) {
+        return x * y * z;
     }
+
+    public double product(double x, double y) {
+        return x * y;
+    }
+}
+
+public static void main(String args[]) {
+Product p= new Product();
+p.product(10,5);
+p.product(10,5,2);
+p.product(5.02,3.01);
+}
+
 }
